e821508d88db181b4208479ffb428eb3114ebc44
Owen Scarpa <154032546+odscarpa@users.noreply.github.com>
2024-06-18 15:46:24 -0600

    added some styling

--- frontend/src/app/about-us/page.tsx
+++ frontend/src/app/about-us/page.tsx
@@ -20,12 +20,12 @@ const AboutUs: React.FC = () => {
             </div>
 
             {/*Who We Are...*/}
-            <div className="py-20 bg-[#E0AF2A] shadow-inner min-h-screen">
+            <div className="py-20 bg-gradient-radial from-amber-400 to-[#E0AF2A] shadow-inner min-h-screen">
                 <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center">
                     <div className="md:w-1/2 p-8">
-                        <h2 className="text-6xl font-bold text-gray-800 mb-4">Who We Are</h2>
-                        <hr className="w-1/2 mb-4 border-t-4 border-amber-400"/>
-                        <div className="bg-white p-4 rounded-xl mb-6">
+                        <h2 className="text-6xl font-bold mb-4 text-white">Who We Are</h2>
+                        <hr className="w-1/2 mb-4 border-t-4 border-black"/>
+                        <div className="bg-white p-4 rounded-xl mb-6 border-8 border-black">
                             <p className="text-gray-700 mb-6 leading-relaxed text-xl font-serif">
                                 Following several years working in various capacities within the New Mexico highway construction market, David "Hossie" Sanchez realized the need for a pavement marking firm that would place a strong emphasis on quality service and customer satisfaction for the local highway contracting community. Believing that he could put together a company that would fulfill that need and more, "Hossie" established San Bar Construction Corp. </p>
                                 <p className={"text-gray-700 mb-6 leading-relaxed text-xl font-serif"}>Since it's beginning in 1990, the Company's stated mission has been; to serve the contracting community as a professional , quality based, service-oriented contacting firm that strives to meet the needs of the construction industry in New Mexico and the Southwest. Today, thanks to to a team of highly qualified personnel, state-of-the-art equipment, and unsurpassed expertise in the field of specialty contracting services, San Bar is recognized throughout the region as a leader in the industry.
@@ -46,12 +46,12 @@ const AboutUs: React.FC = () => {
 
 
             {/* History Section */}
-            <div className="py-20 bg-gradient-to-r from-gray-100 to-gray-200 shadow-inner min-h-screen">
+            <div className="py-20 bg-gradient-to-r from-gray-100 to-gray-250 shadow-inner min-h-screen">
                 <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center">
                     <div className="md:w-1/2 p-8">
                         <h2 className="text-6xl font-bold text-gray-800 mb-4">Our History and What We Do</h2>
                         <hr className="w-1/2 mb-4 border-t-4 border-amber-400"/>
-                        <div className="bg-white p-4 rounded-xl mb-6">
+                        <div className="bg-white p-4 rounded-xl mb-6 border-8 border-black">
                             <p className="text-gray-700 mb-6 leading-relaxed text-xl font-serif">
                                 San Bar Construction Corp. was established in August of 1989 around the kitchen table of
                                 its owner, David “Hossie” Sanchez. After four month of planning and preparation, the
@@ -84,19 +84,19 @@ const AboutUs: React.FC = () => {
 
             {/* Leadership Section */}
             <div
-                className="py-20 bg-gradient-to-r from-gray-200 to-gray-300 shadow-inner h-screen flex flex-col md:flex-row items-center">
+                className="py-20 bg-gradient-radial from-amber-400 to-[#E0AF2A] shadow-inner h-screen flex flex-col md:flex-row items-center">
                 {/* Image block */}
                 <div className="w-full md:w-1/2 p-6 order-1 md:order-none">
                     <div className="w-full h-96 rounded-lg overflow-hidden shadow-md">
                         <img src="/HossieSanchez.JPG" alt="David 'Hossie' Sanchez"
-                             className="w-full h-full object-cover"/>
+                             className="w-full h-full object-cover rounded-lg shadow-2xl transform hover:scale-105 transition duration-300"/>
                     </div>
                 </div>
                 {/* Text block */}
                 <div className="w-full md:w-1/2 p-6">
-                    <h2 className="text-8xl md:text-6xl text-center md:text-left mb-4">Leadership</h2>
-                    <hr className="w-full mb-4 border-t-4 border-amber-400"/>
-                    <div className="p-6 bg-white rounded-2xl mt-6">
+                    <h2 className="text-8xl md:text-6xl text-center md:text-left mb-4 text-white">Leadership</h2>
+                    <hr className="w-full mb-4 border-t-4 border-black"/>
+                    <div className="p-6 bg-white rounded-2xl mt-6 border-8 border-black">
 
                         <p className="text-black leading-relaxed mb-6 text-center md:text-left font-serif text-xl">
 
@@ -108,7 +108,7 @@ const AboutUs: React.FC = () => {
                     </div>
                     <div className="w-full text-center mt-10">
                         <a href="/leadership"
-                           className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full shadow-lg transition duration-300">
+                           className="inline-block bg-black hover:bg-gray-500 text-white font-bold py-3 px-6 rounded-full shadow-lg transition duration-300">
                             Meet the Team
                         </a>
                     </div>
