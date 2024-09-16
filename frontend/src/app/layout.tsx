66574736dc48f5d81ff953684f4a73b0147a9f69
JoeDon McConnell <jdmconnell@gmail.com>
2024-09-16 13:04:33 -0600

    changes to newly deployed website again2

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -14,21 +14,32 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-                                     children,
-                                   }: Readonly<{ children: React.ReactNode; }>) {
-  return (
-      <html lang="en">
-      <head>
-          <link rel="icon" type="image/png" href="/sanbarconst.png" sizes="24x48"/>
-          <meta name="description"
-                content="San Bar Construction Corp specializes in road services, including pavement markings, guardrail installation, sign design, water blasting, and more."/>
-          <title>San Bar Construction Corp | Specialized Construction Company. </title>
-      </head>
-      <body className={bebas.className}>
-      <ResponsiveNav/>
-      {children}
-      <Footer/>
-      </body>
-      </html>
-  );
+                                       children,
+                                   }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang="en">
+        <head>
+            <link rel="icon" type="image/png" href="/sanbarconst.png" sizes="24x36" />
+
+            {/* Title for Google Search */}
+            <title>San Bar Construction Corp | Specialized Construction Company | Sign Manufacturer </title>
+
+            {/* Meta Description for Google Search */}
+            <meta name="description"
+                  content="San Bar Construction Corp specializes in road services, including pavement markings, guardrail installation, sign design, and more." />
+
+            {/* Open Graph Tags for Social Media */}
+            <meta property="og:title" content="San Bar Construction Corp | Road Services & Sign Manufacturing" />
+            <meta property="og:description" content="San Bar Construction Corp specializes in road construction, guardrail installation, and sign manufacturing across New Mexico." />
+            <meta property="og:image" content="/SanBarTextLogo-1.webp" />
+            <meta property="og:url" content="https://www.sanbarcc.com" />
+            <meta property="og:type" content="website" />
+        </head>
+        <body className={bebas.className}>
+        <ResponsiveNav />
+        {children}
+        <Footer />
+        </body>
+        </html>
+    );
 }
