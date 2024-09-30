e68289c296c7e9ce9c83521dea0c001185a91c37
JoeDon McConnell <jdmconnell@gmail.com>
2024-09-30 09:44:46 -0600

    fixing page redirects for non-existing urls in google search until website is recrawled

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            // Redirect old "about" page to the correct "about-us" page
+            {
+                source: '/about',
+                destination: '/about-us',
+                permanent: true,
+            },
+
+            // Redirect old "signs" page to the new "roadway-signs" page
+            {
+                source: '/signs',
+                destination: '/roadway-signs',
+                permanent: true,
+            },
+
+            // Redirect old "Our Staff" link to the correct page
+            {
+                source: '/xxxx',
+                destination: '/leadership', // Assuming this is the correct destination for "Our Staff"
+                permanent: true,
+            },
+
+            // Redirect old guardrail page to the new "Guardrail" page
+            {
+                source: '/guardrail-installation-repair',
+                destination: '/services/Guardrail',
+                permanent: true,
+            },
+
+        ];
+    },
+};
 
 export default nextConfig;
