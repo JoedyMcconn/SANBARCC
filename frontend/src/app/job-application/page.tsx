089697c8e2f90f1b20b6fe08c13f42d188b98f55
JoeDon McConnell <jdmconnell@gmail.com>
2026-08-22 13:41:09 -0600

    removing a job position

--- frontend/src/app/job-application/page.tsx
+++ frontend/src/app/job-application/page.tsx
@@ -215,10 +215,6 @@ export default function JobApplicationForm() {
                     {/*    <input type="checkbox" name="position" value="Acting Foreman" className="mr-2"/>Acting Foreman*/}
                     {/*</label>*/}
 
-                    <label className="flex items-center">
-                        <input type="checkbox" name="position" value="Maintenance Technician"
-                               className="mr-2"/> Maintenance Technician
-                    </label>
 
                     <label className="flex items-center">
                         <input type="checkbox" name="position" value=" CDL Driver" className="mr-2"/> CDL Truck Driver
